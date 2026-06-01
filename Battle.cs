171a5a32697d532e5ea71365e8570ec545a86f0e
Jakubdian <jakubdian2009@gmail.com>
2026-06-01 17:45:17 +0200

    Add files via upload
    Final

---
+++ Battle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HraSK1
+{
+    internal class Battle
+    {
+        public void Utok(Warrior warrior1, Warrior warrior2)
+        {
+            if (warrior1.mood <= 5)
+            {
+                Console.WriteLine(warrior1.name + ": " + "začal/a útočit");
+            }
+
+            if (warrior2.mood <= 5)
+            {
+                Console.WriteLine(warrior2.name + ": " + "začal/a útočit");
+            }
+
+        }
+
+        public void Utok(Mage mage1, Mage mage2)
+        {
+            if (mage1.mood <= 5)
+            {
+                Console.WriteLine(mage1.name + ": " + "začal/a útočit");
+            }
+
+            if (mage2.mood <= 5)
+            {
+                Console.WriteLine(mage2.name + ": " + "začal/a útočit");
+            }
+
+        }
+    }
+}
---
+++ Prace_ze_souborem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace HraSK1
+{
+    internal class Prace_ze_souborem
+    {
+        public string[] Cteni_ze_souboru(string nazevsouboru)
+        {
+            string[] Vety = new string[20];
+            int i = 0;
+
+            using (StreamReader sr = new StreamReader(nazevsouboru))
+            {
+                string s;
+                while ((s = sr.ReadLine()) != null)
+                {
+                    Vety[i] = s;
+                    i++;
+                }
+            return Vety;
+            }
+        }
+    }
+}
